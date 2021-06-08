96822f9a2a13fe6ec7f3c2f4bd395010c6d0acce
chengjhan <chengjhan@gmail.com>
2021-06-09 00:11:21 +0800

    加入 Opera 及 Comment models

---
+++ OperaWeb/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperaWeb.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserComment { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public int OperaID { get; set; }
+
+        public Opera Opera { get; set; }
+    }
+}
